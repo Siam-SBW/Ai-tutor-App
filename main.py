1d1216b846c140cd5402a0b7414e51f66023a76d
Siam-SBW <38445@sbw.ac.th>
2026-05-21 17:41:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ API_KEY = st.secrets["GEMINI_API_KEY"]
 
 genai.configure(api_key=API_KEY)
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.5-flash')
 
 
 
